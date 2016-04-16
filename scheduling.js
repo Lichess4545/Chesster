105487dc5b3a081af02cf9c5347af62385b2b151
Lakin Wecker <lakin@structuredabstraction.com>
2016-04-15 21:03:46 -0600

    Parsing of schedule strings mostly complete.
    This version has tests for all of the most recent round of schedule
    strings. It's probably going to still have some bugs, but I think if we
    wrap it properly in a function that complains when you get the format
    wrong and suggests a specific easy to enter format then people will
    learn quickly.

--- scheduling.js
+++ scheduling.js
@@ -1,30 +1,112 @@
 var moment = require("moment");
-var fuzzy_match = require("fuzzy_match");
+var fuzzy_match = require("./fuzzy_match");
 
 var ISO_TUESDAY = 2;
+var DATE_FORMATS = [
+    "YYYYMMDD HHmm",
+    "YYYYMMDD Hmm",
+    "HHmm DDMMYYYY"
+];
+var WORDS_TO_IGNORE = [
+    "",
+    "-",
+    "–",
+    "at",
+    "on",
+    "gmt",
+    "rescheduled",
+    "reschedule",
+    "reschedule:",
+    "to",
+    "v",
+    "vs",
+    "vs.",
+    "versus",
+    "sunday",
+    "monday",
+    "tuesday",
+    "wednesday",
+    "thursday",
+    "friday",
+    "saturday",
+    "sun",
+    "mon",
+    "tues",
+    "wed",
+    "thu",
+    "thur",
+    "thurs",
+    "fri",
+    "sat",
+
+    // TODO: yes, this is oddly specific - but unless he changes his username ....
+    "steveharwell"
+];
 
 // Make an attempt to parse a scheduling string. 
 //
 // first parameter is the string to parse
 // second parameter is an options dict specifying the round extrema options
 function parse_scheduling(string, options) {
+    // Only going to deal with lowercase
+    string = string.toLowerCase();
+
+    // Remove punctionation.
+    string = string.replace(/[@\.,\(\)]/g, ' ');
+
+    // Split it up
     var parts = string.split(" ");
+
+    // Filter it to remove all the noise
     var filtered_parts = [];
     parts.forEach(function(part) {
         // Remove all lone 
-        if (part == "@") {
-            continue;
-        }
-        if (part == "GMT") {
-            continue;
+        if (WORDS_TO_IGNORE.indexOf(part) != -1) {
+            return;
         }
         filtered_parts.push(part);
     });
-    if (parts != 4) {
-        throw Error("Unable to parse scheduling string: " + string);
+    if (filtered_parts.length < 3) {
+        throw new Error("Unable to parse scheduling string: " + string);
     }
 
+    // Now build up some possible strings and try a bunch of patterns
+    // to find a date in our range.
+    var extrema = get_round_extrema(options);
+    date_string = filtered_parts.slice(2).join(" ");
+    var date_strings = [];
+    date_strings.push(date_string)
+    var now = moment.utc();
+    year = now.year();
+    month = now.month()+1;
+    date_strings.push("" + year + "/" + date_string);
+    date_strings.push("" + year + "/" + month + "/" + date_string);
+
+    var dates = [];
+    date_strings.forEach(function(date_string) {
+        DATE_FORMATS.forEach(function(format) {
+            var date = moment.utc(date_string, format);
+            if (!date.isValid()) {
+                return;
+            }
+            if (date.isBefore(extrema[0]) || date.isAfter(extrema[1])) {
+                return;
+            }
+            dates.push(date);
+        });
+    });
+    if (dates.length < 1) {
+        throw new Error("Couldn't parse date: " + date_strings[0]);
+    }
+
+    // strip out any punctuation from the usernames
+    var white = filtered_parts[0].replace(/[@\.,\(\):]/g, '');
+    var black = filtered_parts[1].replace(/[@\.,\(\):]/g, '');
+    // return the result
     return {
+        white: white,
+        black: black,
+        date: dates[0]
     };
 }
 
--- test/test_scheduling.js
+++ test/test_scheduling.js
@@ -5,6 +5,7 @@ var scheduling = require('../scheduling');
 var ISO_TUESDAY = 2;
 
 describe('scheduling', function() {
+    //--------------------------------------------------------------------------
     describe('#get_round_extrema()', function () {
         it(".isoWeekday() of the bounds should always return 2", function() {
             var bounds = scheduling.get_round_extrema();
@@ -31,24 +32,303 @@ describe('scheduling', function() {
             assert.equal(end.format(), "2016-04-18T23:00:00+00:00")
         });
     });
+    //--------------------------------------------------------------------------
     describe('#parse_scheduling()', function () {
         it("Test team-scheduling messages", function() {
             var options = {
-                containing_date: moment.utc("2015-04-15")
+                containing_date: moment.utc("2016-04-15")
             };
         });
         it("Test lonewolf-scheduling messages", function() {
             var options = {
-                containing_date: moment.utc("2015-04-15"),
+                containing_date: moment.utc("2016-04-15"),
                 offset_hours: 1
             };
-            var test_cases = [
-                ["@autotelic v @explodingllama 4/16 @ 0900 GMT", "2016-04-16:T09:00:00+00:00"],
-            ];
-            test_cases.forEach(function(test_case) {
-                var results = scheduling.parse_scheduling(test_case[0], options);
-                assert.equal(results.date.format(), test_case[1]);
-            });
+            function test_parse_scheduling(string, expected)  {
+                var results = scheduling.parse_scheduling(string, options);
+                assert.equal(results.date.format(), expected.date);
+                assert.equal(results.white, expected.white);
+                assert.equal(results.black, expected.black);
+            }
+            test_parse_scheduling(
+                "@autotelic v @explodingllama 4/16 @ 0900 GMT", {
+                    white: "autotelic",
+                    black: "explodingllama",
+                    date: "2016-04-16T09:00:00+00:00"
+                }
+            );
+            test_parse_scheduling(
+                "@ronaldulyssesswanson – @esolcneveton rescheduled to Sunday, April 17th at 14:00 GMT.",
+                {
+                    white: "ronaldulyssesswanson",
+                    black: "esolcneveton",
+                    date: "2016-04-17T14:00:00+00:00"
+                }
+            );
+            test_parse_scheduling(
+                "@adrianchessnow:  v @mydogeatslemons: 4/15 2300 GMT",
+                {
+                    white: "adrianchessnow",
+                    black: "mydogeatslemons",
+                    date: "2016-04-15T23:00:00+00:00"
+                }
+            );
+            test_parse_scheduling(
+                "@juansnow v @jimcube27 4/17 @ 1030 GMT",
+                {
+                    white: "juansnow",
+                    black: "jimcube27",
+                    date: "2016-04-17T10:30:00+00:00"
+                }
+            );
+            test_parse_scheduling(
+                "@mrrobot v @ashkanjah 4/16 @ 1400 GMT",
+                {
+                    white: "mrrobot",
+                    black: "ashkanjah",
+                    date: "2016-04-16T14:00:00+00:00"
+                }
+            );
+            test_parse_scheduling(
+                "osskjc vs Stoy Fri. 15th 8:00 GMT.",
+                {
+                    white: "osskjc",
+                    black: "stoy",
+                    date: "2016-04-15T08:00:00+00:00"
+                }
+            );
+            test_parse_scheduling(
+                "@ronaldulyssesswanson – @esolcneveton on Saturday, April 16th at 14:00 GMT.",
+                {
+                    white: "ronaldulyssesswanson",
+                    black: "esolcneveton",
+                    date: "2016-04-16T14:00:00+00:00"
+                }
+            );
+            test_parse_scheduling(
+                "@rreyv vs @krzem 4/17 (sunday) 15:00 GMT",
+                {
+                    white: "rreyv",
+                    black: "krzem",
+                    date: "2016-04-17T15:00:00+00:00"
+                }
+            );
+            test_parse_scheduling(
+                "@atrophied vs @jaivl 4/14 20:00",
+                {
+                    white: "atrophied",
+                    black: "jaivl",
+                    date: "2016-04-14T20:00:00+00:00"
+                }
+            );
+            test_parse_scheduling(
+                "reschedule @modakshantanu vs @hakonj April 14th 7:00 GMT",
+                {
+                    white: "modakshantanu",
+                    black: "hakonj",
+                    date: "2016-04-14T07:00:00+00:00"
+                }
+            );
+            test_parse_scheduling(
+                "@quirked vs @vishysoisse 4/14 21:00 GMT",
+                {
+                    white: "quirked",
+                    black: "vishysoisse",
+                    date: "2016-04-14T21:00:00+00:00"
+                }
+            );
+            test_parse_scheduling(
+                "@theino: vs @cactus 4/14 2:30 gmt",
+                {
+                    white: "theino",
+                    black: "cactus",
+                    date: "2016-04-14T02:30:00+00:00"
+                }
+            );
+            test_parse_scheduling(
+                "@seb32 vs @ Petruchio 4/15 23:00 GMT.",
+                {
+                    white: "seb32",
+                    black: "petruchio",
+                    date: "2016-04-15T23:00:00+00:00"
+                }
+            );
+            test_parse_scheduling(
+                "@soldadofiel vs @durchnachtundwind Wednesday 4/13 23.09 GMT",
+                {
+                    white: "soldadofiel",
+                    black: "durchnachtundwind",
+                    date: "2016-04-13T23:09:00+00:00"
+                }
+            );
+            test_parse_scheduling(
+                "@greyhawk vs @immortality  thursday 4/14  2100 GMT",
+                {
+                    white: "greyhawk",
+                    black: "immortality",
+                    date: "2016-04-14T21:00:00+00:00"
+                }
+            );
+            test_parse_scheduling(
+                "@soldadofiel vs @durchnachtundwind Wednesday 4/13 23.09 GMT",
+                {
+                    white: "soldadofiel",
+                    black: "durchnachtundwind",
+                    date: "2016-04-13T23:09:00+00:00"
+                }
+            );
+            test_parse_scheduling(
+                "@nacional100 vs @tnan123 Friday 4/15 @ 14:00 GMT",
+                {
+                    white: "nacional100",
+                    black: "tnan123",
+                    date: "2016-04-15T14:00:00+00:00"
+                }
+            );
+            test_parse_scheduling(
+                "hillrp vs @endrawes0  0100 GMT 17/4/16",
+                {
+                    white: "hillrp",
+                    black: "endrawes0",
+                    date: "2016-04-17T01:00:00+00:00"
+                }
+            );
+            test_parse_scheduling(
+                "@saschlars vs @preserve April 16th at 12:00 GMT",
+                {
+                    white: "saschlars",
+                    black: "preserve",
+                    date: "2016-04-16T12:00:00+00:00"
+                }
+            );
+            test_parse_scheduling(
+                "reschedule: @modakshantanu  vs @hakonj April 14th @ 15:00 GMT",
+                {
+                    white: "modakshantanu",
+                    black: "hakonj",
+                    date: "2016-04-14T15:00:00+00:00"
+                }
+            );
+            test_parse_scheduling(
+                "@ihaterbf vs @hyzer 4/16 22:00 GMT",
+                {
+                    white: "ihaterbf",
+                    black: "hyzer",
+                    date: "2016-04-16T22:00:00+00:00"
+                }
+            );
+            test_parse_scheduling(
+                "@djcrisce vs @zantawb 4/15 00:00 GMT",
+                {
+                    white: "djcrisce",
+                    black: "zantawb",
+                    date: "2016-04-15T00:00:00+00:00"
+                }
+            );
+            test_parse_scheduling(
+                "@jptriton vs @cyanfish Thurs April 14 @ 18:00 GMT",
+                {
+                    white: "jptriton",
+                    black: "cyanfish",
+                    date: "2016-04-14T18:00:00+00:00"
+                }
+            );
+            test_parse_scheduling(
+                "@narud vs @lakinwecker 4/16 17:00 GMT",
+                {
+                    white: "narud",
+                    black: "lakinwecker",
+                    date: "2016-04-16T17:00:00+00:00"
+                }
+            );
+            test_parse_scheduling(
+                "@steveharwell (greentiger) v @harrison2 Apr 15 at 1500 GMT",
+                {
+                    white: "greentiger",
+                    black: "harrison2",
+                    date: "2016-04-15T15:00:00+00:00"
+                }
+            );
+            test_parse_scheduling(
+                "@jyr vs @droodjerky  15/04 at 17:00 GMT",
+                {
+                    white: "jyr",
+                    black: "droodjerky",
+                    date: "2016-04-15T17:00:00+00:00"
+                }
+            );
+            test_parse_scheduling(
+                "@chill5555 vs @doganof 15/04 at 17:00 GMT",
+                {
+                    white: "chill5555",
+                    black: "doganof",
+                    date: "2016-04-15T17:00:00+00:00"
+                }
+            );
+            test_parse_scheduling(
+                "@oldtom v @bramminator  04/17 @ 16:15 GMT",
+                {
+                    white: "oldtom",
+                    black: "bramminator",
+                    date: "2016-04-17T16:15:00+00:00"
+                }
+            );
+            test_parse_scheduling(
+                "@ctorh - @practicedave 18/4@18:00GMT",
+                {
+                    white: "ctorh",
+                    black: "practicedave",
+                    date: "2016-04-18T18:00:00+00:00"
+                }
+            );
+            test_parse_scheduling(
+                "@boviced v @hoxhound April 14, 16:00 GMT",
+                {
+                    white: "boviced",
+                    black: "hoxhound",
+                    date: "2016-04-14T16:00:00+00:00"
+                }
+            );
+            test_parse_scheduling(
+                "@pasternak v @riemannn, April 14, 20:00 GMT",
+                {
+                    white: "pasternak",
+                    black: "riemannn",
+                    date: "2016-04-14T20:00:00+00:00"
+                }
+            );
+            test_parse_scheduling(
+                "@angborxley v @theknug April 17th 13:30 GMT",
+                {
+                    white: "angborxley",
+                    black: "theknug",
+                    date: "2016-04-17T13:30:00+00:00"
+                }
+            );
+            test_parse_scheduling(
+                "@imakethenews v. @daveyjones01  April 15, 22:00 GMT, 18:00 EDT",
+                {
+                    white: "imakethenews",
+                    black: "daveyjones01",
+                    date: "2016-04-15T22:00:00+00:00"
+                }
+            );
+            test_parse_scheduling(
+                "@imakethenews v. @daveyjones01  April 15, 22:00 GMT, 18:00 EDT",
+                {
+                    white: "imakethenews",
+                    black: "daveyjones01",
+                    date: "2016-04-15T22:00:00+00:00"
+                }
+            );
+            test_parse_scheduling(
+                "@modakshantanu v @hakonj April 13th 07:00 GMT",
+                {
+                    white: "modakshantanu",
+                    black: "hakonj",
+                    date: "2016-04-13T07:00:00+00:00"
+                }
+            );
         });
     });
 });
