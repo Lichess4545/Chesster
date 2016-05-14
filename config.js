d7e2f689a6935105d6b40bcb00b83c4ca0143874
Lakin Wecker <lakin@structuredabstraction.com>
2016-05-14 16:35:14 -0600

    Adding a note about the private_key and slack token.

--- config.js
+++ config.js
@@ -1,3 +1,5 @@
+// NOTE: Neither of these files are committed and for good reason.
+//       You must provide your own.
 var token = require("./slack_token.js").token;
 var private_key = require("./service_account_key.js").key;
 
