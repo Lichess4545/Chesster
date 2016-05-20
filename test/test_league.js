22f9ec8e88e1ed684e11a6e89c9caac16e6aa61a
Lakin Wecker <lakin@structuredabstraction.com>
2016-05-19 20:21:30 -0600

    Proper async before command to do the pairing refresh.
    Means we have proper reporting for each of the test cases now. yay.

--- test/test_league.js
+++ test/test_league.js
@@ -31,75 +31,75 @@ var _45_45_LEAGUE_CONF = {
 describe('league', function() {
     //--------------------------------------------------------------------------
     describe('after refreshCurrentRoundSchedules', function () {
-        it("Testing stuff that needs refreshCurrentRoundSchedules()", function(done) {
-            var promises = [];
+        _45_league = new league.League(_45_45_LEAGUE_CONF);
+        before(function(done) {
+            _45_league.refreshCurrentRoundSchedules(function(err, pairings) {
+                done();
+            });
+        });
+        it("Testing refreshCurrentRoundSchedules()", function() {
             this.timeout(15000);
             // Normally I would split this test out into multiples,
             // but this is slow enough that I want to test everything at this
             // point.
-            _45_league = new league.League(_45_45_LEAGUE_CONF);
-            _45_league.refreshCurrentRoundSchedules(function(err, pairings) {
-                // Ensure we got the right amount of pairings
-                assert.equal(pairings.length, 78);
+            // Ensure we got the right amount of pairings
+            assert.equal(_45_league._pairings.length, 78);
 
+            // Ensure that the first pairing is what we expect
+            var first_pairing = _45_league._pairings[0];
+            assert.equal(first_pairing.white.toLowerCase(), "steiger07");
+            assert.equal(first_pairing.black.toLowerCase(), "toddle");
+            assert.equal(first_pairing.scheduled_date.format("MM/DD @ HH:mm"), "04/14 @ 16:00");
+            assert.equal(first_pairing.result, "1-0");
+            assert.equal(first_pairing.url, "http://en.lichess.org/FwYcks48");
 
-                // Ensure that the first pairing is what we expect
-                var first_pairing = pairings[0];
-                assert.equal(first_pairing.white.toLowerCase(), "steiger07");
-                assert.equal(first_pairing.black.toLowerCase(), "toddle");
-                assert.equal(first_pairing.scheduled_date.format("MM/DD @ HH:mm"), "04/14 @ 16:00");
-                assert.equal(first_pairing.result, "1-0");
-                assert.equal(first_pairing.url, "http://en.lichess.org/FwYcks48");
+            // Ensure that the second pairing is what we expect.
+            var second_pairing = _45_league._pairings[1];
+            assert.equal(second_pairing.white.toLowerCase(), "felixnl");
+            assert.equal(second_pairing.black.toLowerCase(), "r-mena");
+            assert.equal(second_pairing.scheduled_date.format("MM/DD @ HH:mm"), "04/17 @ 21:00");
+            assert.equal(second_pairing.result, "");
 
-                // Ensure that the second pairing is what we expect.
-                var second_pairing = pairings[1];
-                assert.equal(second_pairing.white.toLowerCase(), "felixnl");
-                assert.equal(second_pairing.black.toLowerCase(), "r-mena");
-                assert.equal(second_pairing.scheduled_date.format("MM/DD @ HH:mm"), "04/17 @ 21:00");
-                assert.equal(second_pairing.result, "");
+        });
+        it("test findPairing", function() {
+            // Ensure that we can find pairings by username now.
+            function testPairings(pairings) {
+                assert.equal(pairings.length, 1);
+                var pairing = pairings[0];
+                assert.equal(pairing.white, "lakinwecker");
+                assert.equal(pairing.black, "osskjc");
+            }
 
-
-                // Ensure that we can find pairings by username now.
-                function test_pairings(pairings) {
-                    assert.equal(pairings.length, 1);
-                    var pairing = pairings[0];
-                    assert.equal(pairing.white, "lakinwecker");
-                    assert.equal(pairing.black, "osskjc");
+            testPairings(_45_league.findPairing("lakinwecker"));
+            testPairings(_45_league.findPairing("osskjc"));
+            testPairings(_45_league.findPairing("lakinwecker", "osskjc"));
+        });
+        it("test getPairingDetails", function(done) {
+            var promises = [];
+            promises.push(_45_league.getPairingDetails({'name': "made-up"}).then(
+                function(details) {
+                    assert.equal(true, _.isEqual({}, details));
                 }
+            ));
+            promises.push(_45_league.getPairingDetails({'name': "lakinwecker"}).then(
+                function(details) {
+                    assert.equal(details.opponent, "osskjc");
+                    assert.equal(details.color, "white");
+                    assert.equal(details.date.format("YYYY-MM-DD HH:mm"), "2016-04-25 10:30");
+                }
+            ));
+            promises.push(_45_league.getPairingDetails({'name': "jughandle10"}).then(
+                function(details) {
+                    assert.equal(details.opponent, "Cynosure");
+                    assert.equal(details.color, "black");
+                    assert.equal(details.date, undefined);
+                }
+            ));
 
-                test_pairings(_45_league.findPairing("lakinwecker"));
-                test_pairings(_45_league.findPairing("osskjc"));
-                test_pairings(_45_league.findPairing("lakinwecker", "osskjc"));
-
-                var test_cases_run = 0;
-                promises.push(_45_league.getPairingDetails({'name': "made-up"}).then(
-                    function(details) {
-                        ++test_cases_run;
-                        assert.equal(true, _.isEqual({}, details));
-                    }
-                ));
-                promises.push(_45_league.getPairingDetails({'name': "lakinwecker"}).then(
-                    function(details) {
-                        ++test_cases_run;
-                        assert.equal(details.opponent, "osskjc");
-                        assert.equal(details.color, "white");
-                        assert.equal(details.date.format("YYYY-MM-DD HH:mm"), "2016-04-25 10:30");
-                    }
-                ));
-                promises.push(_45_league.getPairingDetails({'name': "jughandle10"}).then(
-                    function(details) {
-                        ++test_cases_run;
-                        assert.equal(details.opponent, "Cynosure");
-                        assert.equal(details.color, "black");
-                        assert.equal(details.date, undefined);
-                    }
-                ));
-
-                return Q.all(promises).then(function() {
-                    done();
-                }, function(error) {
-                    done(error);
-                });
+            return Q.all(promises).then(function() {
+                done();
+            }, function(error) {
+                done(error);
             });
         });
     });
