f292fdf4b7204c954913b869b7a3bc810d3f2e06
Andrew W. Haddad <me@andrewwhaddad.com>
2016-09-04 20:41:31 +0000

    add expiration to the respnse

--- src/chesster.js
+++ src/chesster.js
@@ -141,8 +141,9 @@ function(bot, message) {
         'nominate', //hardcoding this for now
         speaker.name
     ).then(function(jsonResult){
-        bot.reply(message, "Use this link to nominate your choice: " + jsonResult.url);
+        bot.reply(message, "Use this link to nominate your choice: {}".format(jsonResult.url));
         bot.reply(message, "NOTE: this is a private link. Do not share it.");
+        bot.reply(message, "This link will expire at: {}".format(jsonResult.expires));
     }).catch(function(error){
         winston.error("[NOMINATION] private link acquisition failure: {}".format(error));
         bot.reply(message, "I failed to get your private link. Please ask @endrawes0 for help.");
