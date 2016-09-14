1cdfbfe5899389de21943a7ced89c3f34b59606e
Andrew W. Haddad <me@andrewwhaddad.com>
2016-09-13 22:46:21 -0400

    update moderators, links, messages, fix vagrant provisions issue

--- setup/vagrant/vagrant-dependencies.sh
+++ setup/vagrant/vagrant-dependencies.sh
@@ -3,5 +3,5 @@ sudo apt-get install language-pack-en
 curl -sL https://deb.nodesource.com/setup_4.x | sudo -E bash -
 sudo apt-get install -y nodejs
 ln -s /vagrant/ /home/vagrant/chesster
-cd ~/chesster
+cd /home/vagrant/chesster
 sudo npm install
