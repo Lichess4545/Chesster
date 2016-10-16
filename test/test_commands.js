915e994b68e5a509dd3049a02d7a1e52adee609b
Andrew W. Haddad <me@andrewwhaddad.com>
2016-10-15 21:07:56 -0400

    finally finished tests

--- test/test_commands.js
+++ test/test_commands.js
@@ -6,7 +6,7 @@ describe('commands', function(){
         function assertIfCalled(message){
             return function(){ assert(false, message); };
         }
-        it("should tokenize a constant string as an object containing parameters for each constant, of the same name and with the constant as the value", function(){
+        it("tokenize a constant string as an object containing parameters for each constant, of the same name and with the constant as the value", function(){
            return commands.tokenize('this is a test string of constants', [
                'this', 'is', 'a', 'test', 'string', 'of', 'constants'
            ]).then(function(parameters){
@@ -21,7 +21,22 @@ describe('commands', function(){
                });
            });
         });
-        it("should throw an InvalidConstantError if the token does not exactly match the specified constant", 
+        it("tokenize a command with all tokenDescriptors", function(){
+            return commands.tokenize('constant 4 text option1', [
+                'constant', 
+                '{int:intVar}',
+                '{text:textVar}',
+                '{option1|option2}'
+            ]).then(function(parameters){
+                assert.deepEqual(parameters, {
+                    'constant': 'constant',
+                    'intVar': 4,
+                    'textVar': 'text',
+                    'option1': 'option1',
+                });
+            });
+        });
+        it("throw an InvalidConstantError if the token does not exactly match the specified constant", 
             function(){
                 return commands.tokenize('this', ['that']).then(assertIfCalled('tokenize did not throw'))
                     .catch(function(error){
@@ -33,7 +48,7 @@ describe('commands', function(){
                 );
             }
         );
-        it("should throw an InvalidTypeError if I create a variable with an unknown type", 
+        it("throw an InvalidTypeError if I create a variable with an unknown type", 
             function(){
                 return commands.tokenize('dummy', ['{unknown:variableName}'])
                     .then(assertIfCalled(false, 'tokenize did not throw'))
@@ -46,7 +61,7 @@ describe('commands', function(){
                 );
             }
         );
-        it("should throw TooFewTokensError if the number of tokens is less than the number of descriptions", 
+        it("throw TooFewTokensError if the number of tokens is less than the number of descriptions", 
             function(){
                 return commands.tokenize(
                     'only 3 tokens', 
@@ -59,10 +74,38 @@ describe('commands', function(){
                 });
             }
         );
-        it("should throw InvalidTokenDescriptionError if the token is not  one of the valid descriptions", 
+        it("throw InvalidTokenDescriptionError if the tokenDescriptor is not  one of the valid descriptions", 
             function(){
                 return commands.tokenize('dummy', ['inva!idT0kenD3scription'])
-                    .then(assertIfCalled('tokenize did not throw'))
-        });
+                    .then(assertIfCalled('tokenize did not throw')).catch(function(error){
+                    assert(
+                        error instanceof commands.InvalidTokenDescriptionError,
+                        'was expecting InvalidTokenDescritionError, got ' + error.name
+                    );
+                });
+            }
+        );
+        it("throw an InvalidChoiceError if the token is not token is not one of the valid choices", 
+            function(){
+                return commands.tokenize('choice0', ['{choice1|choice2|choice3}'])
+                    .then(assertIfCalled('tokenize did not throw')).catch(function(error){
+                    assert(
+                        error instanceof commands.InvalidChoiceError,
+                        'was expecting InvalidChoiceError, got ' + error.name
+                    );
+                });
+            }
+        );
+        it("throw an InvalidTypeValueError if the value provided cannot be parsed into the specified type", 
+            function(){
+                return commands.tokenize('value', ['{int:variable}'])
+                    .then(assertIfCalled('tokenize did not throw')).catch(function(error){
+                    assert(
+                        error instanceof commands.InvalidTypeValueError,
+                        'was expecting InvalidTypeValueError, got ' + error.name
+                    );
+                });
+            }
+        );
     });
 });
