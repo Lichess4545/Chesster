7edc53abb7875c24a91a11285c3d9ef49902cc44
Lakin Wecker <lakin@wecker.ca>
2016-11-04 19:16:56 -0600

    Fixing the test config to work better for #unstable_bot

--- config/testconfig.js
+++ config/testconfig.js
@@ -33,12 +33,13 @@ config["leagues"]["lonewolf"]["heltour"]["baseEndpoint"] = "https://staging.lich
 config["leagues"]["45+45"]["scheduling"]["channel"] = "unstable_bot";
 config["leagues"]["45+45"]["results"]["channel"] = "unstable_bot";
 config["leagues"]["45+45"]["gamelinks"]["channel"] = "unstable_bot";
-config["leagues"]["45+45"]["alternate"]["channel_id"] = "G2G6URBT5";
+config["leagues"]["45+45"]["alternate"]["channel_id"] = "C0VCCPMJ8";
 config["leagues"]["lonewolf"]["scheduling"]["channel"] = "unstable_bot-lonewolf";
 config["leagues"]["lonewolf"]["results"]["channel"] = "unstable_bot-lonewolf";
 config["leagues"]["lonewolf"]["gamelinks"]["channel"] = "unstable_bot-lonewolf";
 
 config["channel_map"]["G2G6URBT5"] = "45+45";
+config["channel_map"]["C0VCCPMJ8"] = "45+45";
 config["channel_map"]["G0DFRURGQ"] = undefined;
 config["channel_map"]["unstable_bot"] = "45+45";
 config["channel_map"]["unstabled_bot-lonewolf"] = "lonewolf";
