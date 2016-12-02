88b8f5fd3005d60a00d968af8e44a48e0ca0efe9
Lakin Wecker <lakin@lakin.ca>
2016-12-01 20:49:51 -0700

    Put the subscription message formatting near the emitter.

--- src/commands/scheduling.js
+++ src/commands/scheduling.js
@@ -333,6 +333,12 @@ function getRoundExtrema(options) {
     };
 }
 
+// There is not active round
+function replyNoActiveRound(bot, message) {
+    var user = "<@"+message.user+">";
+    bot.reply(message, ":x: " + user + " There is currently no active round. If this is a mistake, contact a mod");
+}
+
 // Can't find the pairing
 function schedulingReplyMissingPairing(bot, message) {
     var user = "<@"+message.user+">";
@@ -517,6 +523,18 @@ function ambientScheduling(bot, message) {
     });
     return deferred.promise;
 }
+
+subscription.register(chesster, 'a-game-is-scheduled', function(target, context) {
+    // TODO: put these date formats somewhere, probably config?
+    var friendlyFormat = "ddd @ HH:mm";
+    target = slack.getSlackUserFromNameOrID(target);
+    var targetDate = context.result.date.clone().utcOffset(target.tz_offset/60);
+    context['yourDate'] = targetDate.format(friendlyFormat);
+    var fullFormat = "YYYY-MM-DD @ HH:mm UTC";
+    context['realDate'] = context.result.date.format(fullFormat);
+    return "{white.name} vs {black.name} in {leagueName} has been scheduled for {realDate}, which is {yourDate} for you.".format(context);
+});
+
 
 module.exports.getRoundExtrema = getRoundExtrema;
 module.exports.parseScheduling = parseScheduling;
