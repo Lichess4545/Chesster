63fb523fbe8b733722c36c1b0647169b11012286
Ben Olden-Cooligan <ben.cyanfish@gmail.com>
2017-09-24 20:18:17 -0400

    Add a "link" command

--- src/commands/privateURLs.js
+++ src/commands/privateURLs.js
@@ -4,6 +4,8 @@
 const moment = require('moment-timezone');
 const winston = require("winston");
 const heltour = require('../heltour.js');
+const league = require('../league.js');
+const _ = require('lodash');
 
 //------------------------------------------------------------------------------
 function nomination(bot, message) {
@@ -20,6 +22,39 @@ function availability(bot, message) {
     bot.reply(message, "Use this link to set your availability: {}".format(message.league.options.links.availability));
 }
 
+//------------------------------------------------------------------------------
+function linkAccounts(bot, message) {
+    var slackUser = bot.getSlackUserFromNameOrID(message.user);
+    var leagues = league.getAllLeagues(bot, bot.config);
+    var heltourOptions = leagues[0].options.heltour;
+    heltour.linkSlack(heltourOptions, message.user, slackUser.profile['display_name'] || slackUser.profile['real_name']).then(function(result) {
+        bot.api.im.open({ user: message.user }, function(err, channel) {
+            if (err) return;
+    
+            var channel = channel.channel.id;
+            var text = "";
+            
+            _.each(result.already_linked, function(username) {
+                text += "Your Slack account is already linked with the Lichess account <https://lichess.org/@/{0}|{0}>.\n".format(username);
+            });
+
+            if (result.already_linked.length > 0) {
+                text += "<{}|Click here> to link another Lichess account.\n".format(result.url);
+            } else {
+                text += "<{}|Click here> to link your Slack and Lichess accounts.\n".format(result.url);
+            }
+    
+            bot.say({
+                channel: channel,
+                text: text,
+                attachments: []
+            });
+        });
+    });
+}
+
+
 exports.nomination = nomination;
 exports.notification = notification;
 exports.availability = availability;
+exports.linkAccounts = linkAccounts;
